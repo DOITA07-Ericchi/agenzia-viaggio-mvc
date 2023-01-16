8c79b9c54fffcc746ddd42d36eba320966c052d3
FranzCalore <franzkaiser1993@gmail.com>
2023-01-16 12:38:03 +0100

    creati i model

---
+++ Models/Destinazione.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bootstrap_travel_agency_MVC.Models
+{
+    public class Destinazione
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string CittaArrivo { get; set; }
+
+        public string? PaeseArrivo { get; set; } //Null se uguale al paese di partenza
+
+        List<Viaggio>? ViaggiPerDestinazione { get; set; }
+
+
+    }
+}
---
+++ Models/Viaggio.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bootstrap_travel_agency_MVC.Models
+{
+    public class Viaggio
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public Destinazione Destinazione { get; set; }
+
+        [Required]
+        public DateTime DataPartenza { get; set; }
+
+        [Required]
+        [Range(1,10)] //Gestiamo viaggi di massimo 10 settimane
+        public int SettimaneViaggio { get; set; }
+
+
+    }
+}
