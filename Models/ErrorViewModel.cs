56a022ec1dc240e01d93cbba8727a087b3703796
Ericchi <git@ericchi.moe>
2023-01-16 12:23:27 +0100

    Creato il progetto MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace bootstrap_travel_agency_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
