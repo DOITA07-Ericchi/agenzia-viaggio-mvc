05c19a803895ee5d712112073a6e06c98c197a21
FranzCalore <franzkaiser1993@gmail.com>
2023-01-16 12:31:44 +0100

    Entity Framework commit

--- obj/Release/net7.0/bootstrap-travel-agency MVC.RazorAssemblyInfo.cs
+++ obj/Release/net7.0/bootstrap-travel-agency MVC.RazorAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("bootstrap-travel-agency MVC")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("bootstrap-travel-agency MVC")]
-[assembly: System.Reflection.AssemblyTitleAttribute("bootstrap-travel-agency MVC")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.ConsolidatedAssemblyApplicationPartFact" +
+    "ory, Microsoft.AspNetCore.Mvc.Razor")]
 
 // Generato dalla classe WriteCodeFragment di MSBuild.
 
