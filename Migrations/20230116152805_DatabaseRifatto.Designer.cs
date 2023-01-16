ebc7c2f3b4f4a763f9f88c441def377f5c098d58
Ericchi <git@ericchi.moe>
2023-01-16 16:33:06 +0100

    Migration

--- Migrations/20230116152805_DatabaseRifatto.Designer.cs
+++ Migrations/20230116152805_DatabaseRifatto.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using bootstrap_travel_agency_MVC.Databeezu;
 
@@ -10,9 +11,11 @@ using bootstrap_travel_agency_MVC.Databeezu;
 namespace bootstraptravelagencyMVC.Migrations
 {
     [DbContext(typeof(TravelContext))]
-    partial class TravelContextModelSnapshot : ModelSnapshot
+    [Migration("20230116152805_DatabaseRifatto")]
+    partial class DatabaseRifatto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20230116152805_DatabaseRifatto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bootstraptravelagencyMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatabaseRifatto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataPartenza",
+                table: "Viaggi");
+
+            migrationBuilder.DropColumn(
+                name: "PaeseArrivo",
+                table: "Destinazioni");
+
+            migrationBuilder.RenameColumn(
+                name: "SettimaneViaggio",
+                table: "Viaggi",
+                newName: "GiorniViaggio");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GiorniViaggio",
+                table: "Viaggi",
+                newName: "SettimaneViaggio");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataPartenza",
+                table: "Viaggi",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaeseArrivo",
+                table: "Destinazioni",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
